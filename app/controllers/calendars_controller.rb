b27fafc9cf1ef76288b435d11a9d1804b327e281
Hirocky23 <key.hiroki23@gmail.com>
2024-05-02 21:02:46 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -28,5 +28,6 @@ class CalendarsController < ApplicationController
       today_plans = plans.select { |plan| plan.date == (@todays_date + x) }.map(&:plan)
     days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
     @week_days.push(days)
+    end
   end
 end
